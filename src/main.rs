21443c32aa293bff4bfdd6ade0d0b98cc7ed7765
cy <chanwit.yimneam@hotmail.com>
2024-12-19 08:58:19 +0700

    Initialize Rust project with basic module structure and input data

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use rust_playground::d;
+
+fn main() {
+    d::d9::run();
+}
+
+
+// use std::fmt;
+
+// trait Bag {
+//     fn display(&self);
+// }
+
+// struct Inventory<T> {
+//     item: T,
+// }
+
+// impl<T> Bag for Inventory<T>
+// where
+//     T: fmt::Display,
+// {
+//     fn display(&self) {
+//         println!("{}", self.item);
+//     }
+// }
+
+// // struct Item {
+// //     name: String,
+// //     amount: i32,
+// // }
+
+// fn main() {
+//     let i = Inventory {
+//         item: 10,
+//     };
+//     i.display();
+// }
+
+// // fn main() {
+// //     let s1 = "hell";
+// //     let s;
+// //     {
+// //         let s2 = "world";
+// //         s = longest(s1, s2);
+// //         println!("The longest string is {}", s);
+// //     }
+
+// //     println!("The longest string is {}", s);
+
+// //     let a = 5;
+// //     let c;
+// //     {
+// //         let b = 10;
+// //         c = &b
+// //     }
+// //     println!("c: {}", c);
+// // }
+
+// // fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+// //     if x.len() > y.len() {
+// //         x
+// //     } else {
+// //         y
+// //     }
+// // }
